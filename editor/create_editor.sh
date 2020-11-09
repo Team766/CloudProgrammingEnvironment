15f29f1ab545c176232d439b84d06b5ea0ee3a27
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-11-09 01:13:59 -0800

    Add missing EDITOR_NAME env var to editor container

--- editor/create_editor.sh
+++ editor/create_editor.sh
@@ -18,6 +18,7 @@ if [ -z "$(docker ps -q --filter "name=${container_name}")" ]; then
 		--env "VIRTUAL_HOST=${domain_name}" \
 		--env "VIRTUAL_PORT=3000" \
 		--env "LETSENCRYPT_HOST=${domain_name}" \
+		--env "EDITOR_NAME=${instance_name}" \
 		--volume "${storage}:/home/project" \
 		--volume /app/ssh:/home/coder/.ssh \
 		--name "$container_name" \

646cfc3c81c89e91ff926c578fb284e1ca9af2c1
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-11-21 08:54:05 -0800

    Split build_and_push_dockers.sh

--- sim/build_and_push_dockers.sh
+++ sim/build_and_push_dockers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Run this script on your machine to deploy a new version of the simulator to a remote simulator VM
+# Run this script on your machine to build and deploy a new version of the simulator to a remote
+# simulator VM.
 #
 # Before running this script, build the simulator in Unity for WebGL and x86-64 server targets.
 # The webgl target should be saved into $sim_build_dir with a name of `webgl` and the server
@@ -15,7 +16,7 @@ sim_build_dir=$2
 # The hostname of the remote simulator VM
 hostname=$3
 
-script_dir="$( dirname "${BASH_SOURCE[0]}" )"
+script_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 docker build -t localhost:5000/frc2020-sim:$version -f $script_dir/server/Dockerfile $sim_build_dir
 
@@ -24,36 +25,4 @@ rm -rf webgl/
 cp -r $sim_build_dir/webgl/ webgl
 docker build -t localhost:5000/frc2020-sim-web:$version .
 
-ssh -N -L 5000:localhost:5000 root@$hostname &
-tunnel_pid=$!
-trap "kill $tunnel_pid" EXIT
-# Wait for SSH to connect
-sleep 10
-
-docker push localhost:5000/frc2020-sim-web:$version
-docker push localhost:5000/frc2020-sim:$version
-
-ssh root@$hostname \
-  docker stop sim
-ssh root@$hostname \
-  docker stop sim-web
-ssh root@$hostname \
-  docker rm sim
-ssh root@$hostname \
-  docker rm sim-web
-
-ssh root@$hostname \
-  docker run -d \
-    --name sim \
-    --restart=always \
-    --log-driver json-file --log-opt max-size=10m --log-opt max-file=10 \
-    -p 1735:1735 -p 1130:1130 -p 1140:1140 -p 5800-5810:5800-5810 -p 7778:7778 \
-    localhost:5000/frc2020-sim:$version
-
-ssh root@$hostname \
-  docker run -d \
-    --name sim-web \
-    --restart=always \
-    --log-driver json-file --log-opt max-size=10m --log-opt max-file=10 \
-    -p 80:80 \
-    localhost:5000/frc2020-sim-web:$version
+$script_dir/push_dockers.sh $version $hostname
---
+++ sim/push_dockers.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Run this script on your machine to deploy a new version of the simulator to a remote simulator VM
+#
+# This script should be run after build_and_push_dockers.sh in order to deploy to additional VMs
+
+set -euo pipefail
+
+# A version string to use to tag the new version of the simulator, such as 0.12.1
+version=$1
+# The hostname of the remote simulator VM
+hostname=$2
+
+ssh -N -L 5000:localhost:5000 root@$hostname &
+tunnel_pid=$!
+trap "kill $tunnel_pid" EXIT
+# Wait for SSH to connect
+sleep 10
+
+docker push localhost:5000/frc2020-sim-web:$version
+docker push localhost:5000/frc2020-sim:$version
+
+ssh root@$hostname \
+  docker stop sim
+ssh root@$hostname \
+  docker stop sim-web
+ssh root@$hostname \
+  docker rm sim
+ssh root@$hostname \
+  docker rm sim-web
+
+ssh root@$hostname \
+  docker run -d \
+    --name sim \
+    --restart=always \
+    --log-driver json-file --log-opt max-size=10m --log-opt max-file=10 \
+    -p 1735:1735 -p 1130:1130 -p 1140:1140 -p 5800-5810:5800-5810 -p 7778:7778 \
+    localhost:5000/frc2020-sim:$version
+
+ssh root@$hostname \
+  docker run -d \
+    --name sim-web \
+    --restart=always \
+    --log-driver json-file --log-opt max-size=10m --log-opt max-file=10 \
+    -p 80:80 \
+    localhost:5000/frc2020-sim-web:$version
